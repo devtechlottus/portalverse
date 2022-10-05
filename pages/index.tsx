c1c050898d942e6c14b8a60145dbd7a2ebef7f25
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-10-05 14:27:33 -0500

    feat: added Links to navigate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <>Diseño
       <Head>
         <title>Portalverse</title>
       </Head>
       <h1 className='text-center'>Welcome to Portalverse</h1>
+      <h2>Programas Disponibles</h2>
+      <ul>
+        <li>
+          <Link href="/educative-offer/design">
+            <a>Diseño</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/educative-offer/code">
+            <a>Código</a>
+          </Link>
+        </li>
+      </ul>
     </>
   )
 }
