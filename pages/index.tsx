a4810a4f8ccd6dd496df2590a9e11e840680a7c4
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-10-04 21:51:13 -0500

    fix: moved title to right place

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <h1 className='text-center'>Welcome to Portalverse</h1>
+    <>
+      <Head>
+        <title>Portalverse</title>
+      </Head>
+      <h1 className='text-center'>Welcome to Portalverse</h1>
+    </>
   )
 }
 
