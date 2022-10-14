d210ebe8b9427363264bb745190da62aa267e6fd
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-10-14 01:46:34 -0500

    feat: added Filter wrapper

---
+++ types/Filter.types.ts
@@ -0,0 +1,21 @@
+export interface ConfigFilter {
+  label: string;
+  icon?: string;
+};
+
+export interface OptionsFilter {
+  value: string;
+  active: boolean;
+  label: string;
+};
+
+export type ConfigItemFilterComponent = {
+  config: ConfigFilter;
+  options: OptionsFilter[];
+}
+
+type FilterComponentData = {
+  [filter: string]: ConfigItemFilterComponent;
+}
+
+export default FilterComponentData;
