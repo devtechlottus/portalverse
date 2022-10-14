d7787f693b2842d3a15a8f0b14b42b3c97261b35
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-10-14 01:41:07 -0500

    fix: removed unnecessary config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-    }
-  },
   images: {
     unoptimized: true
   }
