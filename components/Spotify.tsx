f1f0cbba0d75c5008327ca7f6ce4ce9595614b51
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-10-14 01:45:28 -0500

    feat: added spotify wrapper

---
+++ components/Spotify.tsx
@@ -0,0 +1,18 @@
+import { createRef, FC, useEffect } from "react";
+import SpotifyComponentData from "@/types/Spotify.types";
+
+const Spotify: FC<SpotifyComponentData> = ({ data }: SpotifyComponentData) => {
+
+  const spotifyRef = createRef();
+
+  useEffect(() => {
+    const { config } = data;
+
+    (spotifyRef.current as any).data = { ...config };
+
+  }, [data, spotifyRef])// eslint-disable-line react-hooks/exhaustive-deps
+
+  return <lottus-spotify ref={spotifyRef}></lottus-spotify>
+}
+
+export default Spotify;
