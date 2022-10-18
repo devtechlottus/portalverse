0e5c9f36f17592a7657beb4f86f9c2aa24cbb115
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-10-17 20:37:34 -0500

    feat: added styles configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      spacing: {
+        "12.5": "3.125rem",
+        18: "4.5rem",
+      },
+      fontSize: {
+        "5.5": "1.375rem",
+        "8.5": "2.125rem",
+        13: "3.25rem",
+      },
+      lineHeight: {
+        "8.6": "2.172rem",
+        "16.25": "4.063rem",
+      },
+      colors: {
+        "SC-Actions-AC-200": "#F2F2F2",
+        "UNI-066": "#555555",
+      },
       fontFamily: {
-        "Nunito": ["Nunito"]
+        "Nunito": ["Nunito"],
+        "Poppins": ["Poppins"],
       }
     },
   },
