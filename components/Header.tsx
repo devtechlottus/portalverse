3e4297e322bc3a264f7d92d567adfa9d02a8e23e
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-11-01 12:00:41 -0600

    feat: added lottus tag custom

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,50 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
-import IconComponent from "@/components/Icon";
-import Button from "@/components/Button/Button";
-import { ButtonInit } from "@/components/fixture";
+import { createRef } from "react";
 
 const Header = () => {
+  const navPortalverseRef = createRef();
 
-  const buttonInfo = { ...ButtonInit, title: "Pedir información"};
-
-  return <section className="w-full flex border-b-2 border-solid border-black font-Nunito">
-    <div className="py-6 px-7">
-      <Link href={"/"}>
-        <a>
-          <Image src="https://www.fimpes.org.mx/images/universidades/uane.PNG" width={143} height={38}  alt="logo" />
-        </a>
-      </Link>
-    </div>
-    <div className="grow">
-      <div className="flex justify-end gap-6 py-1">
-        <span>
-          <p>Accesos para:</p>
-        </span>
-        <span>
-          <Link href={"/alumnos"}>
-            <a>Alumnos</a>
-          </Link>
-        </span>
-        <span className="pr-7">
-          <Link href={"/egresados"}>
-            <a>Egresados</a>
-          </Link>
-        </span>
-      </div>
-      <div className="flex py-2">
-        <div className="grow">
-          {/* Aqui van los menus */}
-        </div>
-        <div className="px-4 cursor-pointer">
-          <IconComponent name="search" className="w-6" />
-        </div>
-        <div className="px-8">
-          <Button data={buttonInfo} />
-        </div>
-      </div>
-    </div>
-  </section> 
+  return <lottus-nav-portalverse ref={navPortalverseRef}></lottus-nav-portalverse>
 }
 
 export default Header;
