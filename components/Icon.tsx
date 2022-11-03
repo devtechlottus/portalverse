97d360f36813bd47b65d1d01eedc446def47fd60
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-11-03 15:39:16 -0600

    feat: added new icons

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,7 +1,15 @@
 import SearchIcon from '../public/icons/search.svg';
+import MarkerIcon from '../public/icons/marker.svg';
+import PhoneIcon from '../public/icons/phone.svg';
+import EmailIcon from '../public/icons/email.svg';
+import EyeIcon from '../public/icons/eye.svg';
 
 const iconTypes: any = {
-  search: SearchIcon
+  search: SearchIcon,
+  marker: MarkerIcon,
+  phone: PhoneIcon,
+  email: EmailIcon,
+  eye: EyeIcon,
 };
 
 const IconComponent = ({ name, ...props }: any) => {
