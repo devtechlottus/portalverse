dfdd841134c86f3fd40ee52037989b43887647f2
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-11-07 19:57:34 -0600

    fix: replaced img native instead of Image component

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,11 +1,12 @@
-import { FC } from "react";
-import cn from "classnames";
-import ImageComponentData from "@/types/Image.types";
+import { FC } from "react"
+import Imagen from "next/image"
+import cn from "classnames"
+import ImageComponentData from "@/types/Image.types"
 
 const Image: FC<ImageComponentData> = ({ alt, src, classNames }: ImageComponentData) => {
-  return <div className={cn(classNames)}>
-    <img className='w-full h-full object-fill' alt={alt} src={src} />
+  return <div className={cn("relative", classNames)}>
+    <Imagen src={src} alt={alt} layout="fill" priority />
   </div>
 }
 
-export default Image;
+export default Image
