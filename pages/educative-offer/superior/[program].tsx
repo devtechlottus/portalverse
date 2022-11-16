8f7b3e7cc577ba95a26bed7c5865e587f7b3a6a9
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-11-16 17:16:02 -0600

    feat: refactored grid use and added new functionality

--- pages/educative-offer/superior/[program].tsx
+++ pages/educative-offer/superior/[program].tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect } from "react"
+import { useEffect } from "react"
 import Head from "next/head"
 import Routes from "@/routes/Routes"
 import HeaderFooterLayout from "@/layouts/HeaderFooter.layout"
@@ -9,14 +9,16 @@ import Image from "@/components/Image"
 import Tabs from "@/components/Tabs"
 import ContentInsideLayout from "@/layouts/ContentInside.layout"
 import CardsOstanding from "@/components/CardsOustanding"
-import Table from "@/components/Table"
-import Button from "@/components/Button"
+import ContentFullLayout from "@/layouts/ContentFull.layout"
+import Academia from "@/components/Academia"
+import { SelectInit } from "@/components/fixture"
 
 const EducativeOfferProgram: NextPageWithLayout<any> = ({ data }: any) => {
 
   useEffect(() => {
     console.log("post", data)
-  }, [])// eslint-disable-line react-hooks/exhaustive-deps
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   const dataSuperior = {
     head: {
       title: "Lienciatura en Administración de empresas",
@@ -45,156 +47,153 @@ const EducativeOfferProgram: NextPageWithLayout<any> = ({ data }: any) => {
       initText:"Inicio: ",
       initValue:"1 / Julio /2023 ",
       duracionText:"Duración: ",
-      duracionValue:"10 periodos de 4 meses"
+      duracionValue:"10 periodos de 4 meses",
+    },
+    select: {
+      config: { ...SelectInit, icon: "home", disabled: true },
+      options: [
+        {
+          value: '00',
+          active: true,
+          text: '',
+        },
+        {
+          value: '01',
+          active: false,
+          text: 'Opción 1',
+        },
+        {
+          value: '02',
+          active: false,
+          text: 'Opción 2',
+        },
+        {
+          value: '03',
+          active: false,
+          text: 'Opción 3',
+        },
+        {
+          value: '04',
+          active: false,
+          text: 'Opción 4',
+        },
+      ]
     }
-
   }
   return <>
     <Head>
       <title>Detailr Superior Detail</title>
     </Head>
-    <ContentInsideLayout classNames="gap-6">
-      <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 mt-6">
-        <Breadcum data={{
-            textItems: [],
-            icon: "home",
-            tagOnItem: undefined,
-            tagOnBack: undefined,
-            textColor: undefined
-          }} />
-      </div>
-      <div className="col-span-6 w-t:col-span-8 w-p:col-span-4 w-d:mb-12">
-        <h1 className="text-13 font-bold font-Nunito leading-13 w-t:leading-[111%] w-p:leading-[125%] w-t:text-8.5 w-p:text-7.5">{dataSuperior.head.title}</h1>
-        <p className="text-base font-Nunito leading-5 w-t:leading-[125%] w-p:leading-[125%] w-t:text-sm w-p:text-sm ">{dataSuperior.head.description}</p>
-      </div>
-      <div className="col-span-6 w-t:col-span-8 w-p:col-span-4 w-d:mb-12">
-        <Image
-          alt={dataSuperior.head.image.alt}
-          src={dataSuperior.head.image.src}
-          classNames="aspect-2/1 w-t:aspect-2/1 w-p:aspect-4/3"
-        />
-      </div>
-      <div className="col-span-12 w-t:col-span-8 w-p:col-span-4">
-        <p className="text-6.5 font-Nunito font-semibold leading-[125%] w-t:leading-[125%] w-p:leading-[125%] w-t:text-6 w-p:text-6">{dataSuperior.modality.title}</p>
-      </div>
-      <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 flex justify-center w-d:mb-2">
-        <Tabs data={{items: [{label: 'Item Tab 1', icons: { primary: 'star', duplicate: false,}, disabled: false,},{label: 'Item Tab 1', icons: { primary: 'star', duplicate: false,}, disabled: false,}, {label: 'Item Tab 1', icons: {primary: 'star', duplicate: false, }, disabled: false, }], size: 'medium', }} />
-      </div>
-      <div className="col-span-12 w-t:col-span-8 w-p:col-span-4">
-        <CardsOstanding data={{title: 'Conoce tu futuro perfil',
-          cards: [
-            {
-              image: {
-                desktop:
-                  'https://images.vexels.com/media/users/3/204811/isolated/preview/a307512393af96412f590360aba3fdfd-mujer-feliz-con-car-aacute-cter-de-libros-by-vexels.png',
-                mobile:
-                  'https://fmrockandpop.com/media/k2/items/cache/2ded9d06202dff1f6b8071cb70aba6eb_L.webp?t=20220524_174959',
-              },
-              title: 'Perfil de ingreso 1',
-              text: 'Al estudiar la Licenciatura en Administración de Empresas, serás un profesionista capaz de contribuir en la planeación, organización, dirección y control de los recursos de la empresarial y las tecnologías de la información como herramientas de apoyo para la toma de decisiones, orientando sus acciones hacia el desarrollo del negocio.',
-              backgroundColor: '#8B9BA3',
-            },
-            {
-              image: {
-                desktop:
-                  'https://images.vexels.com/media/users/3/204811/isolated/preview/a307512393af96412f590360aba3fdfd-mujer-feliz-con-car-aacute-cter-de-libros-by-vexels.png',
-                mobile:
-                  'https://fmrockandpop.com/media/k2/items/cache/2ded9d06202dff1f6b8071cb70aba6eb_L.webp?t=20220524_174959',
-              },
-              title: 'Perfil de ingreso 2',
-              text: 'Al estudiar la Licenciatura en Administración de Empresas, serás un profesionista capaz de contribuir en la planeación, organización, dirección y control de los recursos de la empresarial y las tecnologías de la información como herramientas de apoyo para la toma de decisiones, orientando sus acciones hacia el desarrollo del negocio.',
-              backgroundColor: '#6F7C82',
-            },
-            {
-              image: {
-                desktop:
-                  'https://images.vexels.com/media/users/3/204811/isolated/preview/a307512393af96412f590360aba3fdfd-mujer-feliz-con-car-aacute-cter-de-libros-by-vexels.png',
-                mobile:
-                  'https://fmrockandpop.com/media/k2/items/cache/2ded9d06202dff1f6b8071cb70aba6eb_L.webp?t=20220524_174959',
-              },
-              title: 'Perfil de ingreso 1',
-              text: 'Al estudiar la Licenciatura en Administración de Empresas, serás un profesionista capaz de contribuir en la planeación, organización, dirección y control de los recursos de la empresarial y las tecnologías de la información como herramientas de apoyo para la toma de decisiones, orientando sus acciones hacia el desarrollo del negocio.',
-              backgroundColor: '#8B9BA3',
-            },
-            {
-              image: {
-                desktop:
-                  'https://images.vexels.com/media/users/3/204811/isolated/preview/a307512393af96412f590360aba3fdfd-mujer-feliz-con-car-aacute-cter-de-libros-by-vexels.png',
-                mobile:
-                  'https://fmrockandpop.com/media/k2/items/cache/2ded9d06202dff1f6b8071cb70aba6eb_L.webp?t=20220524_174959',
-              },
-              title: 'Perfil de ingreso 2',
-              text: 'Al estudiar la Licenciatura en Administración de Empresas, serás un profesionista capaz de contribuir en la planeación, organización, dirección y control de los recursos de la empresarial y las tecnologías de la información como herramientas de apoyo para la toma de decisiones, orientando sus acciones hacia el desarrollo del negocio.',
-              backgroundColor: '#6F7C82',
-            },
-            
-          ],
-          wrapper: false,}}
-        />
-      </div>
-      <ContentInsideLayout classNames="col-span-12 w-t:col-span-8 w-p:col-span-4">
-        <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 bg-SC-Actions-AC-200">
-          <div className="w-d:py-[72px] w-d:px-[100px] w-t:p-6 w-p:p-6">
-            <h1 className="text-5.5 w-t:text-base w-p:text-base font-Nunito font-bold leading-[130%]">{dataSuperior.searchSection.title}</h1>
-            <p className="text-base w-t:invisible w-p:invisible font-Nunito font-bold leading-[130%]">{dataSuperior.searchSection.subtitle}</p>
-            <p className="text-base w-t:text-3.5 w-p:text-3.5 font-Nunito leading-[125%] w-t:leading-[19px] mt-[17px]">{dataSuperior.searchSection.select.title}</p>
-            <div>
-            Aqui va el select
-            </div>
-          </div>
-          <div className="opacity-25">
-            <div className="grid gap-6 w-d:px-[100px] w-d:pb-[72px] w-t:p-6 w-p:p-6">
-              <div>
-                <p className="text-base w-t:text-3.5 w-p:text-3.5 font-Nunito leading-[125%]">{dataSuperior.searchResult.title}</p>
-                <p className="text-base w-t:text-3.5 w-p:text-3.5 font-Nunito font-bold leading-[130%] w-t:leading-[125%] w-p:leading-[125%]">{dataSuperior.searchResult.plan}</p>
-              </div>
-              <div>
-                <span className="text-base w-t:text-3.5 w-p:text-3.5 leading[125%] font-Nunito">{dataSuperior.searchResult.initText}</span>
-                <span className="text-base w-t:text-3.5 w-p:text-3.5 leading[125%] font-Nunito font-bold">{dataSuperior.searchResult.initValue}</span>
-                <span className="text-base w-t:text-3.5 w-p:text-3.5 leading[125%] font-Nunito">{dataSuperior.searchResult.duracionText}</span>
-                <span className="text-base w-t:text-3.5 w-p:text-3.5 leading[125%] font-Nunito font-bold">{dataSuperior.searchResult.duracionValue}</span>
-              </div>
-            </div>
-            <div className="w-d:px-[100px] w-d:pb-[72px] w-t:p-6 w-p:p-6">
-              <Table data={{
-                head: "Semestre 1",
-                rows: ['Materia 1', 'Materia 2', 'Materia 3'],
-                icon: ""
+    <HeaderFooterLayout>
+      <ContentLayout>
+        <ContentInsideLayout classNames="gap-6">
+          <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 mt-6">
+            <Breadcum data={{
+                textItems: [],
+                icon: "home",
+                tagOnItem: undefined,
+                tagOnBack: undefined,
+                textColor: undefined
               }} />
-            </div>
-            <div className="w-d:px-[100px] w-d:pb-[72px] w-t:p-6 w-p:p-6 flex w-p:flex-col">
-              <Button data={{
-                  id: undefined,
-                  type: undefined,
-                  title: 'undefined',
-                  size: undefined,
-                  icon: undefined,
-                  lyIcon: undefined,
-                  disabled: undefined,
-                  isExpand: undefined,
-                  tagOnClick: undefined,
-                  test: undefined
-                }}/>
-              <Button data={{
-                  id: undefined,
-                  type: undefined,
-                  title: 'undefined',
-                  size: undefined,
-                  icon: undefined,
-                  lyIcon: undefined,
-                  disabled: undefined,
-                  isExpand: undefined,
-                  tagOnClick: undefined,
-                  test: undefined
-                }} />
-            </div>
           </div>
-        </div>
-      </ContentInsideLayout>
-      <div className="order-last col-span-12 col-start-3 col-end-11 w-t:col-span-8 w-p:col-span-4">
-        {/*  */}
-      </div>
-    </ContentInsideLayout> 
+          <div className="col-span-6 w-t:col-span-8 w-p:col-span-4 w-d:mb-12">
+            <h1 className="text-13 font-bold font-Nunito leading-13 w-t:leading-[111%] w-p:leading-[125%] w-t:text-8.5 w-p:text-7.5">{dataSuperior.head.title}</h1>
+            <p className="text-base font-Nunito leading-5 w-t:leading-[125%] w-p:leading-[125%] w-t:text-sm w-p:text-sm ">{dataSuperior.head.description}</p>
+          </div>
+          <div className="col-span-6 w-t:col-span-8 w-p:col-span-4 w-d:mb-12">
+            <Image
+              alt={dataSuperior.head.image.alt}
+              src={dataSuperior.head.image.src}
+              classNames="aspect-2/1 w-t:aspect-2/1 w-p:aspect-4/3"
+            />
+          </div>
+          <div className="col-span-12 w-t:col-span-8 w-p:col-span-4">
+            <p className="text-6.5 font-Nunito font-semibold leading-[125%] w-t:leading-[125%] w-p:leading-[125%] w-t:text-6 w-p:text-6">{dataSuperior.modality.title}</p>
+          </div>
+          <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 flex justify-center w-d:mb-2">
+            <Tabs data={{items: [{label: 'Item Tab 1', icons: { primary: 'star', duplicate: false,}, disabled: false,},{label: 'Item Tab 1', icons: { primary: 'star', duplicate: false,}, disabled: false,}, {label: 'Item Tab 1', icons: {primary: 'star', duplicate: false, }, disabled: false, }], size: 'medium', }} />
+          </div>
+          <div className="col-span-12 w-t:col-span-8 w-p:col-span-4">
+            <CardsOstanding data={{title: 'Conoce tu futuro perfil',
+              cards: [
+                {
+                  image: {
+                    desktop:
+                      'https://images.vexels.com/media/users/3/204811/isolated/preview/a307512393af96412f590360aba3fdfd-mujer-feliz-con-car-aacute-cter-de-libros-by-vexels.png',
+                    mobile:
+                      'https://fmrockandpop.com/media/k2/items/cache/2ded9d06202dff1f6b8071cb70aba6eb_L.webp?t=20220524_174959',
+                  },
+                  title: 'Perfil de ingreso 1',
+                  text: 'Al estudiar la Licenciatura en Administración de Empresas, serás un profesionista capaz de contribuir en la planeación, organización, dirección y control de los recursos de la empresarial y las tecnologías de la información como herramientas de apoyo para la toma de decisiones, orientando sus acciones hacia el desarrollo del negocio.',
+                  backgroundColor: '#8B9BA3',
+                },
+                {
+                  image: {
+                    desktop:
+                      'https://images.vexels.com/media/users/3/204811/isolated/preview/a307512393af96412f590360aba3fdfd-mujer-feliz-con-car-aacute-cter-de-libros-by-vexels.png',
+                    mobile:
+                      'https://fmrockandpop.com/media/k2/items/cache/2ded9d06202dff1f6b8071cb70aba6eb_L.webp?t=20220524_174959',
+                  },
+                  title: 'Perfil de ingreso 2',
+                  text: 'Al estudiar la Licenciatura en Administración de Empresas, serás un profesionista capaz de contribuir en la planeación, organización, dirección y control de los recursos de la empresarial y las tecnologías de la información como herramientas de apoyo para la toma de decisiones, orientando sus acciones hacia el desarrollo del negocio.',
+                  backgroundColor: '#6F7C82',
+                },
+                {
+                  image: {
+                    desktop:
+                      'https://images.vexels.com/media/users/3/204811/isolated/preview/a307512393af96412f590360aba3fdfd-mujer-feliz-con-car-aacute-cter-de-libros-by-vexels.png',
+                    mobile:
+                      'https://fmrockandpop.com/media/k2/items/cache/2ded9d06202dff1f6b8071cb70aba6eb_L.webp?t=20220524_174959',
+                  },
+                  title: 'Perfil de ingreso 1',
+                  text: 'Al estudiar la Licenciatura en Administración de Empresas, serás un profesionista capaz de contribuir en la planeación, organización, dirección y control de los recursos de la empresarial y las tecnologías de la información como herramientas de apoyo para la toma de decisiones, orientando sus acciones hacia el desarrollo del negocio.',
+                  backgroundColor: '#8B9BA3',
+                },
+                {
+                  image: {
+                    desktop:
+                      'https://images.vexels.com/media/users/3/204811/isolated/preview/a307512393af96412f590360aba3fdfd-mujer-feliz-con-car-aacute-cter-de-libros-by-vexels.png',
+                    mobile:
+                      'https://fmrockandpop.com/media/k2/items/cache/2ded9d06202dff1f6b8071cb70aba6eb_L.webp?t=20220524_174959',
+                  },
+                  title: 'Perfil de ingreso 2',
+                  text: 'Al estudiar la Licenciatura en Administración de Empresas, serás un profesionista capaz de contribuir en la planeación, organización, dirección y control de los recursos de la empresarial y las tecnologías de la información como herramientas de apoyo para la toma de decisiones, orientando sus acciones hacia el desarrollo del negocio.',
+                  backgroundColor: '#6F7C82',
+                },
+                
+              ],
+              wrapper: false,}}
+            />
+          </div>
+        </ContentInsideLayout>
+      </ContentLayout>
+
+      <section className="w-d:block w-t:hidden w-p:hidden">
+        <ContentLayout>
+          <ContentInsideLayout classNames="col-span-12 w-t:col-span-8 w-p:col-span-4">
+            <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 w-d:py-[72px] w-d:px-[100px] w-t:px-0 w-t:pt-12 w-t:pb-6 w-p:py-6 bg-SC-Actions-AC-200">
+              <Academia data={dataSuperior.searchSection} result={dataSuperior.searchResult} select={dataSuperior.select} />
+            </div>
+          </ContentInsideLayout>
+        </ContentLayout>
+      </section>
+      
+      <ContentFullLayout classNames="col-span-12 w-t:col-span-8 w-p:col-span-4 px-6 w-d:hidden w-t:block w-p:block bg-SC-Actions-AC-200">
+        <ContentInsideLayout classNames="col-span-12 w-t:col-span-8 w-p:col-span-4">
+          <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 w-d:py-[72px] w-d:px-[100px] w-t:px-0 w-t:pt-12 w-t:pb-6 w-p:py-6">
+            <Academia data={dataSuperior.searchSection} result={dataSuperior.searchResult} select={dataSuperior.select} />
+          </div>
+        </ContentInsideLayout>
+      </ContentFullLayout>
+
+      <ContentLayout>
+        <ContentInsideLayout>
+          <div className="order-last col-span-12 col-start-3 col-end-11 w-t:col-span-8 w-p:col-span-4">
+            {/*  */}
+          </div>
+        </ContentInsideLayout>
+      </ContentLayout>
+    </HeaderFooterLayout>
   </>
 }
 
@@ -224,14 +223,6 @@ export async function getStaticProps(context: any) {
   return {
     props: { data: {...dataProgram} },
   }
-}
-
-EducativeOfferProgram.getLayout = function getLayout(page: ReactElement) {
-  return <HeaderFooterLayout>
-    <ContentLayout>
-      { page }
-    </ContentLayout>
-  </HeaderFooterLayout>
 }
 
 export default EducativeOfferProgram;
