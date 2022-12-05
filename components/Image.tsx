2223887adfb5836f228b92f2a698db387795eace
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-12-05 16:36:16 -0600

    feat: added new prop styles image component

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,9 +3,9 @@ import Imagen from "next/image"
 import cn from "classnames"
 import ImageComponentData from "@/types/Image.types"
 
-const Image: FC<ImageComponentData> = memo(({ alt, src, classNames }: ImageComponentData) => {
+const Image: FC<ImageComponentData> = memo(({ alt, src, classNames, classNamesImg }: ImageComponentData) => {
   return <div className={cn("relative", classNames)}>
-    <Imagen src={src} alt={alt} layout="fill" priority />
+    <Imagen className={cn(classNamesImg)} src={src} alt={alt} layout="fill" priority />
   </div>
 });
 
--- types/Image.types.ts
+++ types/Image.types.ts
@@ -2,6 +2,7 @@ type ImageComponentData = {
   alt: string;
   src: string;
   classNames?: string;
+  classNamesImg?: string;
 };
 
 export default ImageComponentData
