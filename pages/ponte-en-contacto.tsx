5c3c1bd16cce6e83e736783bb96ce9178e2a0413
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-12-05 23:08:13 -0600

    feat: implemented ponte en contacto form

--- pages/ponte-en-contacto.tsx
+++ pages/ponte-en-contacto.tsx
@@ -6,55 +6,61 @@ import NextPageWithLayout from "@/types/Layout.types"
 import Button from "@/components/Button"
 import NewBanner from "@/components/NewBanner"
 import { ButtonInit } from "@/components/fixture"
+import ReasonsToContact from "@/forms/container/ReasonsToContact"
+import Head from "next/head"
 
 const PonteEnContacto: NextPageWithLayout = ({ data }: any) => {
   const router = useRouter();
 
-  return <HeaderFooterLayout>
-    <ContentFullLayout>
-      <ContentInsideLayout classNames="gap-6">
-        <div className="col-span-5 w-t:col-span-8 w-p:col-span-4 bg-darkBlue text-white w-t:pb-6">
-          <ContentInsideLayout>
-            <h1 className="col-span-12 col-start-3 col-end-12 w-t:col-span-8 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 w-p:col-end-5 text-10 w-p:text-[30px] w-p:m-6 font-bold font-Poppins mt-[72px] leading-[125%]">{ data.text.title }</h1>
-            <p className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 text-base font-Poppins w-d:mt-6 w-t:mt-6 w-p:mt-2 w-p:mx-6 leading-[24px] w-t:leading-[125%] w-p:leading-3[130%]"><span className="material-icons mr-2">{ data.text.phone.icon }</span>{ data.text.phone.text }</p>
-            <p className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 text-base font-Poppins w-d:mt-6 w-t:mt-6 w-p:mt-2 w-p:mx-6 leading-[24px] w-t:leading-[125%] w-p:leading-3[130%]"><span className="material-icons mr-2">{ data.text.email.icon }</span>{ data.text.email.text }</p>
-            <p className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 text-base font-Poppins w-d:mt-6 w-t:mt-6 w-p:mt-2 w-p:mx-6 leading-[24px] w-t:leading-[125%] w-p:leading-3[130%]"><span className="material-icons mr-2">{ data.text.schedule.icon }</span>{ data.text.schedule.text }</p>
-            <section className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1">
-                <p className="text-4.5 w-t:text-6 font-Poppins font-bold w-d:mt-6 w-t:mt-6 w-p:mt-2 w-p:mx-6 leading-[125%]">{ data.text.campus.title}</p>
-            </section>
-            <section className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 w-d:mb-18 mt-6">
-            <Button data={data.text.campus.button} />
-            </section>
-            <p className="col-span-12 col-start-3 col-end-11 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1  text-4.5 font-bold font-Poppins w-d:mt-12 w-t:mt-18 w-p:mt-16 w-p:ml-6 leading-[125%]">{ data.text.socialMediaTitle }</p>
-            <section className="grid w-d:grid-cols-4 w-t:grid-cols-4 w-p:grid-cols-4 col-start-3 col-end-11 w-t:col-start-2 w-t:col-end-6 w-p:col-start-1 w-p:col-end-4 w-d:mt-[38px] w-t:mt-[38px] w-p:my-[20px] w-p:ml-6 w-d:mb-[7.5rem] w-t:mb-[6.5rem] w-p:mb-13">
-            {
-              data.text.icons.map((item:any, i:number) => <span key={`span-icons-${i}`} className="material-icons">
-                {item}
-              </span>)
-            }
-            </section>
-          </ContentInsideLayout>
-        </div>
-        <div className="col-span-7 w-t:col-span-8 w-p:col-span-4">
-          <ContentInsideLayout>
-            <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 col-start-3 col-end-11 w-t:col-start-1 w-t:col-end-8 w-p:col-start-1 w-p:col-end-4">
-            Aqui va el formulario Lorem ipsum dolor sit amet consectetur adipisicing elit. Error fuga explicabo quibusdam quidem, similique iusto sunt dolore recusandae, quae, incidunt magnam molestiae minima quasi necessitatibus? Doloribus praesentium vitae accusantium earum?
-
-            </div>
-          </ContentInsideLayout>
-        </div>
-      </ContentInsideLayout>
+  return <>
+    <Head>
+      <title>{ data.title }</title>
+    </Head>
+    <HeaderFooterLayout>
       <ContentFullLayout>
-        <NewBanner 
-          data={data.text.banner } 
-          onBtn={ () =>{
-            router.push(`/directorio`)
-            console.log('hola')
-            } 
-          } />
+        <ContentInsideLayout classNames="gap-6">
+          <div className="col-span-5 w-t:col-span-8 w-p:col-span-4 bg-darkBlue text-white w-t:pb-6">
+            <ContentInsideLayout>
+              <h1 className="col-span-12 col-start-3 col-end-12 w-t:col-span-8 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 w-p:col-end-5 text-10 w-p:text-[30px] w-p:m-6 font-bold font-Poppins mt-[72px] leading-[125%]">{ data.text.title }</h1>
+              <p className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 text-base font-Poppins w-d:mt-6 w-t:mt-6 w-p:mt-2 w-p:mx-6 leading-[24px] w-t:leading-[125%] w-p:leading-3[130%]"><span className="material-icons mr-2">{ data.text.phone.icon }</span>{ data.text.phone.text }</p>
+              <p className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 text-base font-Poppins w-d:mt-6 w-t:mt-6 w-p:mt-2 w-p:mx-6 leading-[24px] w-t:leading-[125%] w-p:leading-3[130%]"><span className="material-icons mr-2">{ data.text.email.icon }</span>{ data.text.email.text }</p>
+              <p className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 text-base font-Poppins w-d:mt-6 w-t:mt-6 w-p:mt-2 w-p:mx-6 leading-[24px] w-t:leading-[125%] w-p:leading-3[130%]"><span className="material-icons mr-2">{ data.text.schedule.icon }</span>{ data.text.schedule.text }</p>
+              <section className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1">
+                  <p className="text-4.5 w-t:text-6 font-Poppins font-bold w-d:mt-6 w-t:mt-6 w-p:mt-2 w-p:mx-6 leading-[125%]">{ data.text.campus.title}</p>
+              </section>
+              <section className="col-span-12 col-start-3 col-end-12 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1 w-d:mb-18 mt-6">
+              <Button data={data.text.campus.button} />
+              </section>
+              <p className="col-span-12 col-start-3 col-end-11 w-t:col-start-2 w-t:col-end-8 w-p:col-start-1  text-4.5 font-bold font-Poppins w-d:mt-12 w-t:mt-18 w-p:mt-16 w-p:ml-6 leading-[125%]">{ data.text.socialMediaTitle }</p>
+              <section className="grid w-d:grid-cols-4 w-t:grid-cols-4 w-p:grid-cols-4 col-start-3 col-end-11 w-t:col-start-2 w-t:col-end-6 w-p:col-start-1 w-p:col-end-4 w-d:mt-[38px] w-t:mt-[38px] w-p:my-[20px] w-p:ml-6 w-d:mb-[7.5rem] w-t:mb-[6.5rem] w-p:mb-13">
+              {
+                data.text.icons.map((item:any, i:number) => <span key={`span-icons-${i}`} className="material-icons">
+                  {item}
+                </span>)
+              }
+              </section>
+            </ContentInsideLayout>
+          </div>
+          <div className="col-span-7 w-t:col-span-8 w-p:col-span-4">
+            <ContentInsideLayout>
+              <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 col-start-3 col-end-11 w-t:col-start-1 w-t:col-end-8 w-p:col-start-1 w-p:col-end-4">
+                <ReasonsToContact />
+              </div>
+            </ContentInsideLayout>
+          </div>
+        </ContentInsideLayout>
+        <ContentFullLayout>
+          <NewBanner 
+            data={data.text.banner } 
+            onBtn={ () =>{
+              router.push(`/directorio`)
+              console.log('hola')
+              } 
+            } />
+        </ContentFullLayout>
       </ContentFullLayout>
-    </ContentFullLayout>
-  </HeaderFooterLayout>
+    </HeaderFooterLayout>
+  </>
 }
 
 // `getStaticPaths` requires using `getStaticProps`
@@ -62,6 +68,7 @@ export async function getStaticProps(context: any) {
   return {
     props: {
       data: {
+        title: "Ponte en Contacto",
         text: {
           title: "Contacto",
           phone: {text: '8004433667700', icon: "phone"},
