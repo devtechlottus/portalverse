2406388f4d167092fab9a86634565c7541482892
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-12-10 13:11:41 -0600

    feat: added icons svg

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -4,6 +4,8 @@ import MarkerIcon from "@/icons/marker.svg"
 import PhoneIcon from "@/icons/phone.svg"
 import EmailIcon from "@/icons/email.svg"
 import EyeIcon from "@/icons/eye.svg"
+import HamburguerIcon from "@/icons/hamburguer.svg"
+import CloseIcon from "@/icons/close.svg"
 
 const iconTypes: any = {
   search: SearchIcon,
@@ -11,6 +13,8 @@ const iconTypes: any = {
   phone: PhoneIcon,
   email: EmailIcon,
   eye: EyeIcon,
+  hamburguer: HamburguerIcon,
+  close: CloseIcon
 };
 
 const IconComponent: FC<any> = memo(({ name, ...props }: any) => {
