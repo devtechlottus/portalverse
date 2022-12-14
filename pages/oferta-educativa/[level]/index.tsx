44f3717878754069213c988a2e1dfceca02db53c
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-12-14 02:08:18 -0600

    fix: cleaned comments code

--- pages/oferta-educativa/[level]/index.tsx
+++ pages/oferta-educativa/[level]/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Head from "next/head"
 import Link from "next/link"
 import cn from "classnames"
@@ -6,17 +7,11 @@ import HeaderFooterLayout from "@/layouts/HeaderFooter.layout"
 import NextPageWithLayout from "@/types/Layout.types"
 import Routes from "@/routes/Routes"
 import ContentFullLayout from "@/layouts/ContentFull.layout"
-import licenciatura from "@/dummy/licenciatura"
-import especialidad from "@/dummy/especialidad"
-import doctorado from "@/dummy/doctorado"
-import maestria from "@/dummy/maestria"
-import bachillerato from "@/dummy/bachillerato"
 import Image from "@/components/Image"
 import ContentInsideLayout from "@/layouts/ContentInside.layout"
 import { getDataPageFromJSON } from "@/utils/getDataPage"
-import { useEffect, useState } from "react"
 
-// const Level: NextPageWithLayout<any> = ({ data: { level, config: { title, banner: { image, description: descriptionBanner }, head: { title: titleHead, description: descriptionHead } }, programs } }: any) => {
+
 const Level: NextPageWithLayout<any> = ({ meta, filtro, programs, sections, level }: any) => {
 
   const [ configPrograms, setConfigPrograms ] = useState<Array<any>>([]);
@@ -78,24 +73,10 @@ export async function getStaticPaths() {
 export async function getStaticProps(context: any) {
   const { params: { level } } = context;
   const { meta, filtro, programs, sections } = await getDataPageFromJSON(`/oferta-educativa/${level}.json`);
-  console.log("levelSelected", level)
-  console.log("meta", meta)
-  console.log("filtro", filtro)
-  console.log("programs", programs)
-  // const diccionario: any = {
-  //   licenciatura,
-  //   especialidad,
-  //   doctorado,
-  //   maestria,
-  //   bachillerato,
-  // };
-  // const infoLevel: any = diccionario[levelSelected];
-  // const { config, programs } = Routes["oferta-educativa"].reduce((prev: any, { params: { level, programs, config } }: any) => level === levelSelected ? { ...prev, config: { ...prev.config, ...config }, programs: [ ...prev.programs, ...programs ] } : { ...prev }, { config: {}, programs: [] });
-  // const infoPrograms = programs.reduce((prev: any, { params: { program: route } }: any) => [ ...prev, { route, info: infoLevel[route] } ], []);
+
   return {
     props: {
-      meta, filtro, programs, sections, level 
-      /*data: { level: levelSelected, config, programs: [...infoPrograms]}*/
+      meta, filtro, programs, sections, level
     },
   }
 }
