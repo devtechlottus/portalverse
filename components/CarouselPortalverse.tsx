a84cbbf15c3fcb46c09b7218170784f5177a7bb1
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-12-16 12:09:54 -0600

    feat: added classes and mode

--- components/CarouselPortalverse.tsx
+++ components/CarouselPortalverse.tsx
@@ -2,9 +2,10 @@ import { FC, SyntheticEvent, useEffect, useState } from "react"
 import cn from "classnames"
 import Button from "@/components/Button"
 
-const CarouselPortalverse: FC<any> = ({ classNames, classNamesContainer, items }: any) => {
+const CarouselPortalverse: FC<any> = ({ classNames, classNamesContainer, items, mode = "banner" }: any) => {
 
-  const styleBaseControls = "material-icons cursor-pointer text-[12px] select-none";
+  const styleBaseContainerControls = "top-[35%] w-p:hidden absolute group cursor-pointer"
+  const styleBaseControls = "material-icons text-[12px] select-none group-hover:text-white";
 
   const [ active, setActive ] = useState<number>(0);
   const [ countItems, setCountItems ] = useState<number>(0);
@@ -40,10 +41,10 @@ const CarouselPortalverse: FC<any> = ({ classNames, classNamesContainer, items }
 
   const handleClickBullet = (bullet: number) => setActive(bullet);
 
-  return <section className={cn("relative flex flex-col", classNames)}>
-    <div className={cn("h-full", classNamesContainer)} >
-      {
-        items.map(({ image: { src }, title = "", subtitle = "", action = null }: any, i: number) => <div className={cn("w-full h-full", { "hidden": i !== active })} key={`image-${i}`}>
+  const renderModeContent = (items: Array<any>) => {
+    switch(mode) {
+      case "banner":
+        return items.map(({ image: { src }, title = "", subtitle = "", action = null }: any, i: number) => <div className={cn("w-full h-full", { "hidden": i !== active })} key={`image-${i}`}>
             <div className={cn("w-full", "h-100 w-p:h-90 text-center font-Poppins")} style={{"backgroundImage":`url(${src})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
               { !!title ? <h1 className="top-[50px] pt-12 font-bold text-[65px] leading-[80px] w-p:hidden">{ title }</h1> : null }
               { !!subtitle ? <p className="pt-2 font-semibold text-6 leading-[30px] w-p:hidden">{ subtitle }</p> : null }
@@ -56,36 +57,37 @@ const CarouselPortalverse: FC<any> = ({ classNames, classNamesContainer, items }
               }
             </div>
           </div>
-        )
+        ) 
+        break
+    }
+  } 
+
+  return <section className={cn("relative flex flex-col", classNames)}>
+    <div className={cn("h-full", classNamesContainer)} >
+      {
+        
       }
-      <div className="w-p:hidden absolute left-[40px] top-[45%]">
+      {/* controls */}
+      <div className={cn("left-[40px]", styleBaseContainerControls)}>
         <span aria-label="prev" onClick={handlerClickControl} className={cn(styleBaseControls)}>arrow_back_ios</span>
       </div>
-      <div className="w-p:hidden absolute right-[40px] top-[45%]">
+      <div className={cn("right-[40px]", styleBaseContainerControls)}>
         <span aria-label="next" onClick={handlerClickControl} className={cn(styleBaseControls)}>arrow_forward_ios</span>
       </div>
+      {/* controls */}
       <div className="mb-10">
       {
-        items.map(({ title = "", subtitle = "", action = null }: any, i: number) => <div className={cn("w-full h-full", { "hidden": i !== active })} key={`image-${i}`}>
-            { !!title ? <h1 className="pt-6 font-semibold text-[32px] leading-[40px]">{ title }</h1> : null }
-            { !!subtitle ? <p className="pt-2 font-semibold text-6 leading-[30px]">{ subtitle }</p> : null }
-            {
-              !!action
-                ? <div className={cn("flex justify-end pt-6")}>
-                    <lottus-link-icons data={{  text: 'xdxd', size: 'small', isBold: false, disabled: false, id: '123', icon: 'person',}} />
-                  </div>
-                : null
-            }
-          </div>
-        )
+        renderModeContent(items)
       }
       </div>
     </div>
+    {/* bullets */}
     <div className="w-full flex justify-center gap-2 absolute bottom-[40px] w-p:bottom-0">
       {
         items.map((_: any, i:number) => <div onClick={() => handleClickBullet(i)} className={cn("w-4 h-4 rounded-full bg-[#686868] cursor-pointer", { "w-[32px]": i === active })} key={`bullet-${i}`} />)
       }
     </div>
+    {/* bullets */}
   </section>
 }
 
