da49856113ae29cb344b68ed995c9bf885d042d2
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2022-12-28 20:20:28 -0600

    feat: added new functionalities Filter component

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -1,48 +1,66 @@
 import { FC, memo, useEffect, useState } from "react"
-import FilterComponentData from "@/types/Filter.types"
 import FilterDropdown from "@/components/FilterDropdown/FilterDropdown"
 import { LinkIconsInit } from "@/components/fixture"
 import LinkIcons from "@/components/LinkLottus"
+import FilterComponentConfig from "@/types/Filter.types"
 
-const Filter: FC<FilterComponentData> = memo(({ data }: FilterComponentData) => {
+const Filter: FC<FilterComponentConfig> = memo(({ data, color = "#000", onSelectionItems, onChangeView }: FilterComponentConfig) => {
 
+  const [ active, setActive ] = useState<boolean>(false);
   const [ config, setConfig ] = useState<any[]>([])
-  const [ selectedOptions, setSelectedOptions ] = useState<string[][]>([]);
+  const [ selectedOptions, setSelectedOptions ] = useState<any>({});
+  const [ mosaicActive, setMosaicActive ] = useState<boolean>(true);
+
   const linkLottusConfig = { ...LinkIconsInit, text: 'Eliminar filtros', iconFirst: 'close' };
 
+  const changeMosaicView = () => {
+    const newView = !mosaicActive;
+    setMosaicActive(newView);
+    onChangeView(newView);
+  }
+
   useEffect(() => {
-    const items = Object.entries(data).map(([ key, config ]: any) => ({ key, config }))
-    setConfig((state: any[]) => [ ...items ]);
-  }, [data])
+    const items = Object.entries(data).map(([ key, config ]: any) => ({ key, config }));
+    setConfig([ ...items ]);
+  }, [data]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    if (active) {
+      onSelectionItems(selectedOptions);
+    }
+  }, [selectedOptions]);
 
   const clearAllFilters = () => {
     // clear options
     console.log("its a clear filters")
   }
 
-  const handleOnSelectedOptions = (options: string[], key: string, position: number) => {
-    console.log("data in handle", options, key, position)
+  const handleOnSelectedOptions = (options: string[], key: string) => {
+    setSelectedOptions((state: any) => ({ ...state, [key]: [ ...options ] }));
+    setActive(true);
   }
 
-  return <>
-    <section className="container-filter">
-      <div>
+  return <section className="container-filter w-full flex flex-col">
+      <div className="flex">
         <p>Filtrar programas:</p>
         <span>
           <LinkIcons data={linkLottusConfig} onClick={clearAllFilters}/>
         </span>
       </div>
-      <section>
+      <section className="flex py-3 w-t:flex-col w-p:flex-col">
         {
           config.map( ({ key, config }: any, i: number) =>
-            <div key={`filter-${i}`}>
-              <FilterDropdown data={config} onSelectedOptions={(options: string[]) => handleOnSelectedOptions(options, key, i)} />
+            <div key={`filter-${i}`} className="px-1 flex flex-col">
+              <FilterDropdown color={color} data={config} onSelectedOptions={(options: string[]) => handleOnSelectedOptions(options, key)} onClearOptions={() => {}} />
             </div>
           )
         }
       </section>
+      <div className="flex items-center justify-end cursor-pointer" onClick={changeMosaicView}>
+        <span className="mr-1">Ver en { mosaicActive ? "mosaico" : "lista" }</span>
+        <span className="material-icons icon">{ mosaicActive ? "space_dashboard" : "format_list_bulleted" }</span>
+      </div>
     </section>
-  </>
 });
 
 export default Filter
