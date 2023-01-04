3ef023ea7cc9adcbbdaa27992fc9aa82502ccd4e
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2023-01-04 12:53:25 -0600

    feat: implemented SliderPortalverse

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,10 +4,9 @@ import HeaderFooterLayout from "@/layouts/HeaderFooter.layout"
 import ContentFullLayout from "@/layouts/ContentFull.layout"
 import NextPageWithLayout from "@/types/Layout.types"
 import { getDataPageFromJSON } from "@/utils/getDataPage"
-import BannerPortalverse from "@/components/BannerPortalverse"
 import ContentLayout from "@/layouts/Content.layout"
 import CardWebsite from "@/components/CardWebsite"
-import Slider from "@/components/Slider"
+import Slider from "@/components/SliderPortalverse"
 
 const Blog: NextPageWithLayout = ({ sections, meta }: any) => {
   const router = useRouter()
@@ -19,12 +18,12 @@ const Blog: NextPageWithLayout = ({ sections, meta }: any) => {
     <HeaderFooterLayout breadcrumbs={false}>
 			<ContentFullLayout classNames="gap-6 w-d:hidden">
         <div className="col-span-12 w-t:col-span-8 w-p:col-span-4">
-          <Slider data={{ ...sections.head.banner, height: "350px" }} />
+          <Slider data={{ ...sections.head.banner }} />
         </div>
       </ContentFullLayout>
 			<ContentLayout classNames="mt-12">
         <div className="col-span-12 w-t:col-span-8 w-p:col-span-4">
-          <Slider data={{ ...sections.head.banner, height: "350px" }} />
+          <Slider data={{ ...sections.head.banner, height: "600px" }} />
         </div>
 				<div className="col-span-12 w-t:col-span-8 w-p:col-span-4">
 					<p className="font-Poppins font-bold text-8.5 w-t:text-6 w-p:text-6 leading-[111%] w-t:leading-[125%] w-p:leading-[125%]">{sections.blogNotices.title}</p>
