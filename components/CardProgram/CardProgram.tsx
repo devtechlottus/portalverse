ccab053662cf857891db141900cdf831e33c56df
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2023-01-05 13:19:27 -0600

    feat: added new styles CardPorgram Component

--- components/CardProgram/CardProgram.tsx
+++ components/CardProgram/CardProgram.tsx
@@ -7,24 +7,18 @@ import LinkIcons from "@/components/LinkLottus"
 
 const CardProgram: FC<CardProgramData> = memo(({ title, link, image, classNames , onClick}: CardProgramData) => {
 
-  return <>
-  <div className={cn("bg-white border-1 border-solid border rounded-md", classNames)}>
-    <div > 
+  return <div className={cn("cardProgram bg-white border-1 border-solid border rounded-md", classNames)}> 
       {
         !!image
           ? <Image classNames="aspect-4/3 w-full" alt="contact-image" src={image} />
           : <div className="bg-[gray] rounded w-22 h-22" />
       }
-      </div>
-      <div >
-        <p className="font-Poppins font-bold text-4.5 mt-3 mb-2 mx-3">{title}</p>
-      </div>
-      <div className="flex justify-end pb-2">
+      <p className="font-Poppins font-bold text-4.5 mt-3 mb-2 mx-3">{title}</p>
+      <div className="flex justify-end pb-2 items-center">
         <LinkIcons data={link} onClick={onClick}/>
+        <span className="material-icons icon">chevron_right</span>
       </div>
     </div>
-    
-  </>
 })
 
 export default CardProgram
