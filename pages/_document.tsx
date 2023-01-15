1a56197dba730acb614220a941eaf5f54cbac951
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2023-01-15 14:55:00 -0600

    feat: added link configurations

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      </Head>
       <body>
         <Main />
         <NextScript />
