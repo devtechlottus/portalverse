f78094d4c7147ddb47bc5399ae4aa03145df57c1
pedro.fragoso <pedro.fragoso@lottuseducation.com>
2023-01-15 01:10:39 -0600

    feat: added webpack build analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -6,13 +8,24 @@ const nextConfig = {
     unoptimized: true,
     domains: ['drive.google.com','shutterstock.com','www.uane.edu.mx']
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-    return config;
-  }
+  webpack: (config, { isServer }) => {
+    // console.log(process.env.ANALYZE)
+    // if (!!process.env.ANALYZE) {
+    //  config.plugins.push(
+    //    new BundleAnalyzerPlugin({
+    //      analyzerMode: 'server',
+    //      analyzerPort: isServer ? 8888 : 8889,
+    //      openAnalyzer: true,
+    //    })
+    //  )
+    // } else {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      })
+    // }
+    return config
+  },
 }
 
 module.exports = nextConfig
