f6db4219cbd61d9a3b43c9acd322de4351d808d5
alan llamas <alanllamasg@gmail.com>
2023-01-25 12:48:26 -0600

    chore: commented contentful

--- pages/educacion-continua/[program]/index.tsx
+++ pages/educacion-continua/[program]/index.tsx
@@ -43,15 +43,15 @@ const DetalleCursoEducacionContinua: NextPageWithLayout = ({ sections, meta }: a
         <div className="col-span-12 w-t:col-span-8 w-p:col-span-4 w-d:hidden w-t:hidden hidden">
           <p className="font-Poppins font-bold text-10 pt-6 w-t:text-8.5 w-p:text-7.5 leading-[125%]">{sections.relatedCourses.title}</p>
         </div>
-        <section className="col-span-12 w-t:col-span-8 w-p:col-span-4 w-d:grid-cols-4 gap-6 w-t:grid-cols-2 w-p:grid-cols-1 mb-12 w-d:hidden w-t:hidden hidden">
+        {/* <section className="col-span-12 w-t:col-span-8 w-p:col-span-4 w-d:grid-cols-4 gap-6 w-t:grid-cols-2 w-p:grid-cols-1 mb-12 w-d:hidden w-t:hidden hidden">
           {
            sections.relatedCourses.courses.map((item:any, i:number) => <section key={`section-educativeProgram-${i}`}>
             <CardProgram image={item.image.desk} title={item.title} link={item.link} onClick={()=> router.push(`/${item.redirect}`)}/>
            </section>)
           }
-        </section>
+        </section> */}
       </ContentLayout>
-      <ContentFullLayout classNames="bg-SC/Backgrounds/BG-GRAY w-p:hidden hidden">
+      {/* <ContentFullLayout classNames="bg-SC/Backgrounds/BG-GRAY w-p:hidden hidden">
         <ContentLayout>
           <div className="col-span-12 w-t:col-span-8 w-p:col-span-4">
             <p className="font-Poppins font-bold text-10 pt-6">{sections.relatedCourses.title}</p>
@@ -64,7 +64,7 @@ const DetalleCursoEducacionContinua: NextPageWithLayout = ({ sections, meta }: a
             }
           </section>
         </ContentLayout>
-      </ContentFullLayout>
+      </ContentFullLayout> */}
     </HeaderFooterLayout>
   </>
 }
