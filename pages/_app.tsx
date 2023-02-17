715542445a5c85855bc830aae534dd8d705644e6
alan llamas <alanllamasg@gmail.com>
2023-02-17 16:09:13 -0600

    fix: moved scripts to app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import Script from "next/script"
 import { useRouter } from "next/router"
+import Head from 'next/head'
 import "@/styles/globals.scss"
 import { AppPropsWithLayout } from "@/types/Layout.types"
 import * as gtag from "@/lib/gtag"
@@ -32,9 +33,12 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      </Head>
       <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1ZX45TXRD5" />
-      <Script
-        id='google-analytics'
+      <Script id='google-analytics'
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -45,9 +49,42 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
               page_path: window.location.pathname,
             });
           `,
-          }}
-        />
-          <Component {...pageProps} />
+      }}/>
+      <Script id='google-tag-manager'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NGBCMHM');`,
+      }}/>
+      <Script id='hot-jar'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3340067,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+      }}/>
+      <Script id='meta-pixel'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1ZX45TXRD5', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}/>
+
+      <Component {...pageProps} />
     </>)
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,49 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { Html, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="es">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-       
-          {/* // Hotjar Tracking Code for UANE */}
-         <script dangerouslySetInnerHTML={{ __html: `(function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3340067,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}}>
-          </script>
-
-          {/* // // Meta Pixel Code */}
-          <script dangerouslySetInnerHTML={{ __html : `!function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '487461332128996');
-          fbq('track', 'PageView');`}}>
-          </script>
-          <noscript><img height="1" width="1" style={{"display": "none"}}
-          src="https://www.facebook.com/tr?id=487461332128996&ev=PageView&noscript=1"
-          /></noscript>
-          {/* // //End Meta Pixel Code */}
-
-          {/* // // Google Tag Manager */}
-          <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NGBCMHM');`}}> 
-          </script>
-          {/* // // End Google Tag Manager */}
-      </Head>
       <body>
         {/* // // Meta Pixel Code (noscript) */}
         <noscript><img height="1" width="1" style={{"display":"none","visibility":"hidden"}} src="https://www.facebook.com/tr?id=487461332128996&amp;ev=PageView&amp;noscript=1"/></noscript>
