df01110820651fda09728c4c77c3e22a6187e22f
alan llamas <alanllamasg@gmail.com>
2023-02-17 15:49:02 -0600

    fix: added noscript at body beggining

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,6 +45,9 @@ export default function Document() {
           {/* // // End Google Tag Manager */}
       </Head>
       <body>
+        {/* // // Meta Pixel Code (noscript) */}
+        <noscript><img height="1" width="1" style={{"display":"none","visibility":"hidden"}} src="https://www.facebook.com/tr?id=487461332128996&amp;ev=PageView&amp;noscript=1"/></noscript>
+        {/* // //End Meta Pixel Code (noscript) */}
         {/* // // Google Tag Manager (noscript) */}
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGBCMHM"
         height="0" width="0" style={{"display":"none","visibility":"hidden"}}></iframe></noscript>
