a436ced7eea2e5d0f25b85a9484abd014e524bd0
alan llamas <alanllamasg@gmail.com>
2023-03-09 15:34:44 -0600

    chore: removed head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,62 +33,61 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <>
-      <Head>
-        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1ZX45TXRD5" />
-        <Script id='google-analytics'
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-1ZX45TXRD5', {
-                page_path: window.location.pathname,
-              });
-            `,
-        }}/>
-        <Script id='google-tag-manager'
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1ZX45TXRD5" />
+      <Script id='google-analytics'
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NGBCMHM');`,
-        }}/>
-        <Script id='hot-jar'
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:3340067,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-        }}/>
-        <Script id='meta-pixel'
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-1ZX45TXRD5', {
-                page_path: window.location.pathname,
-              });
-            `,
-        }}/>
-        {/* // // Meta Pixel Code (noscript) */}
-        <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style={{"display":"none","visibility":"hidden"}}
-          src="https://www.facebook.com/tr?id=487461332128996&amp;ev=PageView&amp;noscript=1"/>` }}/>
-        {/* // //End Meta Pixel Code (noscript) */}
-        {/* // // Google Tag Manager (noscript) */}
-        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGBCMHM" height="0"
-          width="0" style={{"display":"none","visibility":"hidden"}}></iframe>` }}/>
-        {/* // // End Google Tag Manager (noscript) */}
-      </Head>
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1ZX45TXRD5', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}/>
+      <Script id='google-tag-manager'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NGBCMHM');`,
+      }}/>
+      <Script id='hot-jar'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3340067,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+      }}/>
+      <Script id='meta-pixel'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1ZX45TXRD5', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}/>
+      {/* // // Meta Pixel Code (noscript) */}
+      <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style={{"display":"none","visibility":"hidden"}}
+        src="https://www.facebook.com/tr?id=487461332128996&amp;ev=PageView&amp;noscript=1"/>` }}/>
+      {/* // //End Meta Pixel Code (noscript) */}
+      {/* // // Google Tag Manager (noscript) */}
+      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGBCMHM" height="0"
+        width="0" style={{"display":"none","visibility":"hidden"}}></iframe>` }}/>
+      {/* // // End Google Tag Manager (noscript) */}
+   
 
       <Component {...pageProps} />
     </>)
