4ed8106f5522f55c0453e46356421a20969b5c5d
alan llamas <alanllamasg@gmail.com>
2023-03-09 15:45:49 -0600

    chore: removed strategy and modified scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,9 +33,8 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-1ZX45TXRD5" />
+      <Script  src="https://www.googletagmanager.com/gtag/js?id=G-1ZX45TXRD5" />
       <Script id='google-analytics'
-        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
@@ -47,7 +46,6 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           `,
       }}/>
       <Script id='google-tag-manager'
-      strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -56,7 +54,6 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
         })(window,document,'script','dataLayer','GTM-NGBCMHM');`,
       }}/>
       <Script id='hot-jar'
-        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `(function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -68,31 +65,37 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
       }}/>
       <Script id='meta-pixel'
-        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;
+          n.push=n;
+          n.loaded=!0;
+          n.version='2.0';
+          n.queue=[];
+          t=b.createElement(e);
+          t.async=!0;
+          t.src=v;
+          s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
           fbq('init', '487461332128996');
           fbq('track', 'PageView');
           `,
       }}/>
+      <Script id='meta-pixel-registration'
+        dangerouslySetInnerHTML={{
+          __html: `
+          fbq('track', 'CompleteRegistration');          
+          `,
+      }}/>
       {/* // // Meta Pixel Code (noscript) */}
-      <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
-        src="https://www.facebook.com/tr?id=487461332128996&amp;ev=PageView&amp;noscript=1"/>` }}/>
+      <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=487461332128996&ev=PageView&noscript=1"/>` }}/>
       {/* // //End Meta Pixel Code (noscript) */}
       {/* // // Google Tag Manager (noscript) */}
       <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGBCMHM" height="0"
         width="0" style={{"display":"none","visibility":"hidden"}}></iframe>` }}/>
       {/* // // End Google Tag Manager (noscript) */}
-   
-
+  
       <Component {...pageProps} />
     </>)
 }
