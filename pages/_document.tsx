a1f0b1cae3c99ae11c69484d93311601ac2f75f4
alan llamas <alanllamasg@gmail.com>
2023-06-09 18:49:43 -0500

    chore: removed comment from head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,14 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        {/* <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=487461332128996&ev=PageView&noscript=1`}
-          />
-        </noscript> */}
+
       </Head>
       
       <body>
