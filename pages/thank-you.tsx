0d5f03095630b111ffebfc769602f9b8d3d24513
alan llamas <alanllamasg@gmail.com>
2023-06-10 06:57:11 -0500

    feat: added pixels to index and thank you pages

--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -37,10 +37,23 @@ const ThankYouPage: NextPageWithLayout = ({ sections, meta }: any) => {
     setFlow('gral')
   }, [router])
 
-  
+  const pixelData: { script?: string, pixel?: string } = {
+    script: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;
+    n.push=n;
+    n.loaded=!0;
+    n.version='2.0';
+    n.queue=[];
+    t=b.createElement(e);
+    t.async=!0;
+    t.src=v;
+    s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');`,
+    pixel:'https://www.facebook.com/tr?id=487461332128996&ev=CompleteRegistration'
+  }
 
-  
   return <>
+    <Pixel {...pixelData}  />
     <Head>
       <title>{ meta.title }</title>
     </Head>
