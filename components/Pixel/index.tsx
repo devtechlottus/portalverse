c0ce2a8409405d71918dff22aa8f2c28a6e9ef8c
alan llamas <alanllamasg@gmail.com>
2023-06-09 21:31:16 -0500

    feat: made props optional for pixel

--- components/Pixel/index.tsx
+++ components/Pixel/index.tsx
@@ -2,20 +2,23 @@
 import React from 'react'
 import Head  from 'next/head'
 
-export default ({script, pixel}: { script: string, pixel: string }) => {
+export default ({script, pixel}: { script?: string, pixel?: string }) => {
 
   return(
     <Head>
       <>
-        <script dangerouslySetInnerHTML={{ __html: script }}/>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={pixel}
-          />
-        </noscript>
+        { !!script && <script dangerouslySetInnerHTML={{ __html: script }}/>}
+
+        { !!pixel &&
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={pixel}
+              />
+          </noscript>
+        }
       </>
     </Head>
   )
