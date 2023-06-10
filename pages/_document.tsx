e41728e1eb8cfffd20b678e03a634887711344ab
alan llamas <alanllamasg@gmail.com>
2023-06-10 06:09:59 -0500

    chore: moved pixel to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+import Pixel from "@/components/Pixel"
 import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
@@ -7,7 +8,20 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       </Head>
-      
+      <Pixel 
+        script={`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;
+          n.push=n;
+          n.loaded=!0;
+          n.version='2.0';
+          n.queue=[];
+          t=b.createElement(e);
+          t.async=!0;
+          t.src=v;
+          s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');`}
+          pixel="https://www.facebook.com/tr?id=487461332128996&ev=PageView"
+      />
       <body>
         <Main />
         <NextScript />
