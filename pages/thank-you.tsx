f0847c8f41e790e03688ec6ac17b4b86d881c804
alan llamas <alanllamasg@gmail.com>
2023-06-10 09:13:53 -0500

    chore: returned pageview track

--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -53,7 +53,9 @@ const ThankYouPage: NextPageWithLayout = ({ sections, meta }: any) => {
         t.async=!0;
         t.src=v;
         s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');`}
+        s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '487461332128996');
+        fbq('track', 'PageView');`}
       pixel="https://www.facebook.com/tr?id=487461332128996&ev=PageView&noscript=1"
       ID="meta-pageview-thankyou"
     />
