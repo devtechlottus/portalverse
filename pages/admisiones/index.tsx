8935640b24a41271bef81a5b46b442f910b8d760
alan llamas <alanllamasg@gmail.com>
2023-06-10 07:36:24 -0500

    chore:added script to where pixel is used

--- pages/admisiones/index.tsx
+++ pages/admisiones/index.tsx
@@ -20,6 +20,7 @@ import RichtText from "@/old-components/Richtext/Richtext"
 import CardWebsitePortalverse from "@/old-components/CardWebsitePortalverse"
 import Icon from "@/old-components/Icon"
 import Pixel from "@/components/Pixel"
+import Script from "next/script"
 
 const LandindAdmissions: NextPageWithLayout = ({ sections, meta }: any) => {
 
@@ -37,6 +38,23 @@ const LandindAdmissions: NextPageWithLayout = ({ sections, meta }: any) => {
   const navigate = (route: string) => router.push(route)
 
   return <>
+  <Script id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;
+          n.push=n;
+          n.loaded=!0;
+          n.version='2.0';
+          n.queue=[];
+          t=b.createElement(e);
+          t.async=!0;
+          t.src=v;
+          s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '487461332128996');`,
+        }
+      }/>
     <Pixel 
       pixel="https://www.facebook.com/tr?id=487461332128996&ev=PageView"
     />
