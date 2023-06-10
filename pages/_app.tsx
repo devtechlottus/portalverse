74732d1995e4f31a03b59a0058946f6ed6151c6a
alan llamas <alanllamasg@gmail.com>
2023-06-10 05:19:00 -0500

    chore: commented pixel script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <>
-      <Pixel 
+      {/* <Pixel 
         script={`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
           if(!f._fbq)f._fbq=n;
           n.push=n;
@@ -31,7 +31,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           t.src=v;
           s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');`}
-      />
+      /> */}
       <Component {...pageProps} />
     </>)
 }
