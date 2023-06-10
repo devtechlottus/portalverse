e8ca655bd80044a95fb6fc456826df6726fdca8f
alan llamas <alanllamasg@gmail.com>
2023-06-10 09:05:24 -0500

    chore: removed track page view

--- pages/admisiones/index.tsx
+++ pages/admisiones/index.tsx
@@ -50,8 +50,7 @@ const LandindAdmissions: NextPageWithLayout = ({ sections, meta }: any) => {
         t.src=v;
         s=b.getElementsByTagName(e)[0];
         s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '487461332128996');
-        fbq('track', 'PageView');`}
+        fbq('init', '487461332128996');`}
       pixel="https://www.facebook.com/tr?id=487461332128996&ev=PageView&noscript=1"
       ID="meta-pageview-admisiones"
     />
