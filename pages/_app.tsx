6e4eba5c93ac13e3fbdc93040c967307feeedd91
alan llamas <alanllamasg@gmail.com>
2023-06-10 01:29:13 -0500

    chore: removed effect for pixel events

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,20 +16,6 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     }
   }, [])
 
-  useEffect(() => {
-    // This pageview only triggers the first time (it's important for Pixel to have real information)
-    // fbq.pageview()
-
-    const handleRouteChange = () => {
-      fbq.pageview()
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-    
-  }, [router.events])
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
