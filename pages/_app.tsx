b2219316edf21b2258cf4881a705cb2f93b64b97
alan llamas <alanllamasg@gmail.com>
2023-06-10 07:08:01 -0500

    chore: split pixel and script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import "@/styles/globals.scss"
 import { AppPropsWithLayout } from "@/types/Layout.types"
 import { useRouter } from "next/router"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const router = useRouter()
@@ -19,6 +20,22 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return getLayout(
     <>
+    <Script
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;
+          n.push=n;
+          n.loaded=!0;
+          n.version='2.0';
+          n.queue=[];
+          t=b.createElement(e);
+          t.async=!0;
+          t.src=v;
+          s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '487461332128996');`,
+        }
+      }/>
       <Component {...pageProps} />
     </>)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -78,20 +78,8 @@ const Home: NextPageWithLayout = ({ data: { sections, meta, strapi } }: any) => 
   return <>
     {/* <CustomScript {...gtag} /> */}
     <Pixel 
-        script={`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;
-          n.push=n;
-          n.loaded=!0;
-          n.version='2.0';
-          n.queue=[];
-          t=b.createElement(e);
-          t.async=!0;
-          t.src=v;
-          s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '487461332128996');`}
-        pixel="https://www.facebook.com/tr?id=487461332128996&ev=PageView"
-      />
+      pixel="https://www.facebook.com/tr?id=487461332128996&ev=PageView"
+    />
     <CustomScript {...hotjar} />
     <Head>
       <title>{meta.title}</title>
