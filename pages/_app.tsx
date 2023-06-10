5b45b075e04f4c0dc8d0dad97557bbd0c4efbfda
alan llamas <alanllamasg@gmail.com>
2023-06-10 06:56:50 -0500

    chore: removed pixel router approach

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,6 @@ import { useEffect } from "react"
 import "@/styles/globals.scss"
 import { AppPropsWithLayout } from "@/types/Layout.types"
 import { useRouter } from "next/router"
-import * as fbq from '../lib/fb-pixel'
-import Pixel from "@/components/Pixel"
-import Script from "next/script"
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const router = useRouter()
@@ -17,42 +14,11 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     }
   }, [])
 
-  useEffect(() => {
-    // This pageview only triggers the first time (it's important for Pixel to have real information)
-    fbq.pageview()
-
-    const handleRouteChange = () => {
-      fbq.pageview()
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-    
-  }, [router.events])
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return getLayout(
     <>
-      <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '487461332128996');
-          `,
-        }}
-      />
       <Component {...pageProps} />
     </>)
 }
