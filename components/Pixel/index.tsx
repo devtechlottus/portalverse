6fe62cf73a1be0c517ffc266ca347a5e5d367926
alan llamas <alanllamasg@gmail.com>
2023-06-09 23:45:42 -0500

    chore: returned no script

--- components/Pixel/index.tsx
+++ components/Pixel/index.tsx
@@ -10,12 +10,14 @@ export default ({script, pixel}: { script?: string, pixel?: string }) => {
         { !!script && <script dangerouslySetInnerHTML={{ __html: script }}/>}
 
         { !!pixel &&
+          <noscript>
             <img
               height="1"
               width="1"
               style={{ display: 'none' }}
               src={pixel}
               />
+          </noscript>
         }
       </>
     </Head>
