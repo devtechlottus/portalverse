0c2e84bc8f4a08fd82f7680dc2fb982045b0e26b
alan llamas <alanllamasg@gmail.com>
2023-06-10 06:56:39 -0500

    chore: removed pixel layout

--- layouts/Content.layout.tsx
+++ layouts/Content.layout.tsx
@@ -4,8 +4,6 @@ import PixelLayout from "./Pixel.layout"
 
 export default function ContentLayout({ children, classNames } : ContentLayoutProps) {
   return <section className={cn("grid grid-cols-12-gap w-t:grid-cols-8-gap w-p:grid-cols-4-gap gap-grid-gap max-w-d-base mx-auto w-d-base:px-6", classNames)}>
-    <PixelLayout>
       { children }
-    </PixelLayout>
   </section>
 }
