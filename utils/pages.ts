91be60f88f94735bddda7b9d4cdd3c7d1250761e
Cris <hecn@live.com.mx>
2023-08-25 17:48:30 -0600

    fix: pages

--- utils/pages.ts
+++ utils/pages.ts
@@ -23,36 +23,22 @@ export const getPageType = async (path: string): Promise<PageType> => {
   const normalizedPath = normalizePath(path);
 
   const blogEntryPageData = await getBlogEntryPageData();
-  const blogEntryParentSlug = normalizePath(
-    blogEntryPageData?.data?.attributes?.slug
-  );
-  const isBlogEntryPage =
-    path?.startsWith(`${blogEntryParentSlug}/`) &&
-    normalizedPath !== normalizePath(blogEntryParentSlug);
+  const blogEntryParentSlug = normalizePath(blogEntryPageData?.data?.attributes?.slug);
+  const isBlogEntryPage = path?.startsWith(`${blogEntryParentSlug}/`) && normalizedPath !== normalizePath(blogEntryParentSlug);
 
   const levelsConfig = await getEducationalOfferingConfig();
-  const programDetailParentSlugs = levelsConfig?.map((levelConfig) =>
-    normalizePath(levelConfig?.slug)
-  );
-  const isProgramDetailPage = programDetailParentSlugs?.reduce(
-    (acc, parentSlug) => {
-      return (
-        acc ||
-        (normalizedPath?.startsWith(`${parentSlug}/`) &&
-          normalizedPath !== parentSlug)
-      );
-    },
-    false
-  );
+  const programDetailParentSlugs = levelsConfig?.map(levelConfig => normalizePath(levelConfig?.slug));
+  const isProgramDetailPage = programDetailParentSlugs?.reduce((acc, parentSlug) => {return acc || normalizedPath?.startsWith(`${parentSlug}/`) && normalizedPath !== parentSlug;
+  }, false);
 
-  if (isBlogEntryPage) {
-    return "blogEntry";
-  } else if (isProgramDetailPage) {
-    return "programDetail";
+  if(isBlogEntryPage) {
+    return "blogEntry"
+  } else if(isProgramDetailPage) {
+    return "programDetail"
   } else {
-    return "dynamic";
+    return "dynamic"
   }
-};
+}
 
 /**
  * PAGE DATA FETCHING
@@ -90,68 +76,53 @@ export type ProgramDetailPage =
 
 export const getPageDataBySlug = async (slug: string) => {
   const pagesInfo = await getPagesInfo();
-
+    
   const targetPage = pagesInfo?.find(
     (page) => normalizePath(page?.attributes?.slug) === normalizePath(slug)
   );
   const targetPageId = targetPage?.id;
 
   if (!targetPageId) throw new Error("Page ID Not Found");
-
+    
   const pageData = await getPageDataById({ id: targetPageId });
   return pageData?.page;
 };
 
-export const getProgramDetailPageData = async (
-  path: string
-): Promise<ProgramDetailPage> => {
+export const getProgramDetailPageData = async (path: string): Promise<ProgramDetailPage> => {
   const pathSegments = normalizePath(path)?.split("/");
-  const levelSlug = pathSegments?.slice(
-    pathSegments?.length - 2,
-    pathSegments?.length - 1
-  )?.[0];
-  const programSlug = pathSegments?.slice(
-    pathSegments?.length - 1,
-    pathSegments?.length
-  )?.[0];
+  const levelSlug = pathSegments?.slice(pathSegments?.length - 2, pathSegments?.length - 1)?.[0];
+  const programSlug = pathSegments?.slice(pathSegments?.length - 1, pathSegments?.length)?.[0];
 
   const staticProgramsByLevel = Routes["oferta-educativa"];
   //@ts-ignore
-  const continuousEducationPrograms =
-    Routes["extension-universitaria"]?.params?.programs || [];
+  const continuousEducationPrograms = Routes["extension-universitaria"]?.params?.programs || [];
 
   const isContinuousEducationProgram = levelSlug === "extension-universitaria";
 
   const isStaticProgram = isContinuousEducationProgram
+    //@ts-ignore
     ? continuousEducationPrograms?.some((program) => {
+        //@ts-ignore
         return program?.params?.program === programSlug;
       })
     : staticProgramsByLevel?.some((level) => {
         return level?.params?.programs?.some((program) => {
-          return (
-            level?.params?.level === levelSlug &&
-            program?.params?.program === programSlug
-          );
+          return level?.params?.level === levelSlug && program?.params?.program === programSlug;
         });
       });
 
   if (isStaticProgram) {
     if (isContinuousEducationProgram) {
-      const { sections, meta } = await getDataPageFromJSON(
-        `extension-universitaria/${programSlug}.json`
-      );
+      const { sections, meta } = await getDataPageFromJSON(`extension-universitaria/${programSlug}.json`);
 
       return {
         type: "StaticContinuousEducationProgramDetail",
-        data: { sections, meta },
+        data: { sections, meta }
       };
+
     } else {
-      const { meta, config, sections, form } = await getDataPageFromJSON(
-        `/oferta-educativa/${levelSlug}/${programSlug}.json`
-      );
-      const bannerParche = await getDataPageFromJSON(
-        "oferta-educativa/oferta-educativa.json"
-      );
+      const { meta, config, sections, form } = await getDataPageFromJSON(`/oferta-educativa/${levelSlug}/${programSlug}.json`);
+      const bannerParche = await getDataPageFromJSON("oferta-educativa/oferta-educativa.json");
 
       return {
         type: "StaticProgramDetail",
@@ -163,13 +134,13 @@ export const getProgramDetailPageData = async (
           sections,
           form,
           bannerParche,
-        },
+        }
       };
+
     }
   } else {
     const programData = await getProgramBySlug(programSlug);
-    const programLevel =
-      programData?.attributes?.level?.data?.attributes?.title;
+    const programLevel = programData?.attributes?.level?.data?.attributes?.title;
     const programDetailSuperior = await getProgramDetailSuperiorPageData();
     const programDetailBachillerato = await getProgramDetailBachillerato();
     return {
@@ -177,14 +148,13 @@ export const getProgramDetailPageData = async (
       type: "DynamicProgramDetail",
       data: {
         program: { ...programData },
-        layout:
-          programLevel === "Bachillerato"
-            ? { ...programDetailBachillerato }
-            : { ...programDetailSuperior },
+        layout: programLevel === "Bachillerato" ? {...programDetailBachillerato} : {...programDetailSuperior}
       },
     };
+
   }
-};
+  
+}
 
 /**
  * PAGES PATHS
@@ -205,9 +175,9 @@ export const getBlogEntryPagesPaths = async () => {
         ?.map((blogEntrySlug) => `${blogEntryParentSlug}/${blogEntrySlug}`)
         ?.filter(isValidPath)
     : [];
-
+  
   return blogEntriesPaths;
-};
+}
 
 export const getDynamicPagesPaths = async () => {
   const pagesInfo = await getPagesInfo();
@@ -215,30 +185,27 @@ export const getDynamicPagesPaths = async () => {
 
   // pages with an invalid path format are filtered out and won't be generated at build time
   const dynamicPagesPaths = pagesPaths?.filter(isValidPath);
-
+  
   return dynamicPagesPaths;
-};
+}
 
 export const getJSONProgramsPaths = async () => {
   const educationalOfferingParams = Routes["oferta-educativa"];
   const continuousEducationParams = Routes["extension-universitaria"];
-  let continuousEducationStaticCategories: Array<StaticContinuousEducationCategory> =
-    [];
+
+  let continuousEducationStaticCategories: Array<StaticContinuousEducationCategory> = [];
+
   try {
-    const continuousEducationStaticPageData = await getDataPageFromJSON(
-      "extension-universitaria/extension-universitaria.json"
-    );
-    continuousEducationStaticCategories = continuousEducationStaticPageData
-      ?.sections?.extension
-      ?.sections as Array<StaticContinuousEducationCategory>;
+    const continuousEducationStaticPageData = await getDataPageFromJSON('extension-universitaria/extension-universitaria.json');
+    continuousEducationStaticCategories = continuousEducationStaticPageData?.sections?.extension?.sections as Array<StaticContinuousEducationCategory>;
+    
   } catch (error) {
     // continuousEducationStaticCategories remains empty
   }
 
   const hiddenContinuousEducationProgramsPaths =
     continuousEducationStaticCategories
-      ?.reduce((programs, category) => {
-        // merge all programs from all categories
+      ?.reduce((programs, category) => { // merge all programs from all categories
         return [...programs, ...category?.cursos];
       }, [] as Array<StaticContinuousEducationProgram>)
       ?.filter((program) => program?.hidden)
@@ -246,53 +213,54 @@ export const getJSONProgramsPaths = async () => {
 
   const paths: Array<string> = [];
 
-  for (const levelParams of educationalOfferingParams) {
-    const staticProgramsData = await getDataPageFromJSON(
-      `/oferta-educativa/${levelParams?.params?.level}.json`
-    );
+  for (const levelParams of educationalOfferingParams){
+
+    const staticProgramsData = await getDataPageFromJSON(`/oferta-educativa/${levelParams?.params?.level}.json`);
     const staticPrograms = staticProgramsData?.programs as Array<StaticProgram>;
 
     const programsParams = levelParams?.params?.programs;
 
-    for (const program of programsParams) {
+    for (const program of programsParams){
       const path = `${levelParams?.params?.levelRoute}/${program?.params?.program}`;
 
       const programSlug = program?.params?.program;
-      const isProgramAvailable = !staticPrograms?.find(
-        (staticProgram) => staticProgram?.route === programSlug
-      )?.config?.hidden;
+      const isProgramAvailable = !staticPrograms?.find(staticProgram => staticProgram?.route === programSlug)?.config?.hidden
 
       if (isProgramAvailable) paths?.push(path);
     }
   }
 
   // Generate paths for every non hidden static Continuous Education program.
+
+  //@ts-ignore
   continuousEducationParams?.params?.programs
+    //@ts-ignore
     ?.filter((program) => {
-      return !hiddenContinuousEducationProgramsPaths?.includes(
-        program?.params?.program
-      );
+      //@ts-ignore
+      return !hiddenContinuousEducationProgramsPaths?.includes(program?.params?.program)
     })
+    //@ts-ignore
     ?.forEach((program) => {
+      //@ts-ignore
       const path = `extension-universitaria/${program?.params?.program}`;
       paths?.push(path);
     });
 
   return paths;
-};
+}
 
 export const getDynamicProgramsPaths = async () => {
   const levelsConfig = await getEducationalOfferingConfig();
 
   const programsPaths: Array<string> = [];
 
-  for (const levelConfig of levelsConfig) {
+  for (const levelConfig of levelsConfig){
     const levelSlug = levelConfig?.slug;
     const normalizedLevelSlug = normalizePath(levelSlug);
     const level = levelConfig?.level?.data?.attributes?.title;
     const programs = await getProgramsByLevel(level);
 
-    for (const program of programs) {
+    for (const program of programs){
       const programSlug = program?.attributes?.slug;
       const programPath = `${normalizedLevelSlug}/${programSlug}`;
       programsPaths?.push(programPath);
@@ -300,20 +268,17 @@ export const getDynamicProgramsPaths = async () => {
   }
 
   return programsPaths;
-};
+}
 
 export const getProgramDetailPagesPaths = async () => {
   const strapiProgramDetailPagesPaths = await getDynamicProgramsPaths();
   const staticProgramDetailPagesPaths = await getJSONProgramsPaths();
 
-  const paths = [
-    ...strapiProgramDetailPagesPaths,
-    ...staticProgramDetailPagesPaths,
-  ]?.map(normalizePath);
+  const paths = [...strapiProgramDetailPagesPaths, ...staticProgramDetailPagesPaths]?.map(normalizePath);
   const set = Array.from(new Set(paths)); // remove duplicates
 
   return set;
-};
+}
 
 /**
  * PAGES BREADCRUMBS
